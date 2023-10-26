50566a5549d9bfc49dc520012c952cb18bca04b1
alona-maksymova <alona.maksymova@care.com>
2023-10-26 22:29:11 +0300

    Marshal/Unmarshall

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type person struct {
+	FirstName string
+	LastName  string
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	p1 := person{
+		FirstName: "Alona",
+		LastName:  "Maksymova",
+	}
+	p2 := person{
+		FirstName: "Polina",
+		LastName:  "Maksymova",
+	}
+	xp := []person{p1, p2}
+	bc, err := json.Marshal(xp)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println("JSON string", string(bc))
+
+	xp2 := []person{}
+	err = json.Unmarshal(bc, &xp2)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("Objects slice", xp2)
 }
