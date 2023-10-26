2b164b2e77ae14df87694aee97125dcfe7cca18f
alona-maksymova <alona.maksymova@care.com>
2023-10-26 23:05:04 +0300

    Decode

--- main.go
+++ main.go
@@ -51,5 +51,11 @@ func foo(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func bar(w http.ResponseWriter, r *http.Request) {
+	var p1 person
+	err := json.NewDecoder(r.Body).Decode(&p1)
+	if err != nil {
+		log.Println("Issue with decoding!!!")
+	}
+	log.Println("Person", p1)
 
 }
